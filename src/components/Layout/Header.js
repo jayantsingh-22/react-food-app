e5e6ff9a1bc8e28de1dbec4704af3a5523009305
jayantsingh-22 <jayantsingh640@gmail.com>
2023-06-17 23:00:44 +0530

    added:components folder in src dir

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import foodImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>PiattoSquisito</h1>
+        <HeaderCartButton />
+      </header>
+
+      <div className={classes["main-image"]}>
+        <img src={foodImage} alt="A glance of our dishes." />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
