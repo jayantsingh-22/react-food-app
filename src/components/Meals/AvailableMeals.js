ecbb3fdce8a91aedb1fd19014a43d4187adb3f30
jayantsingh-22 <jayantsingh640@gmail.com>
2023-06-19 21:23:47 +0530

    pushed changes

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -34,11 +34,12 @@ const AvalilableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
     />
-  )); 
+  ));
 
   return (
     <section className={classes.meals}>
